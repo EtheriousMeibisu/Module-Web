e80e505bebad83fe64ce64d72e029ff1331879c9
Yulya Lukicheva <104934302+DrowsyAlchemist@users.noreply.github.com>
2023-06-22 12:37:50 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    final static int PORT = 9999;
+
+    public static void main(String[] args) {
+
+Server server = new Server();
+server.startingTheServer(PORT);
+
+
+    }
+}
