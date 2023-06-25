1b38361592ac61ad7cdf9bffa1bddf5d7f974f77
Yulya Lukicheva <104934302+DrowsyAlchemist@users.noreply.github.com>
2023-06-25 14:43:57 +0300

    Handlers*

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,28 @@
 package org.example;
 
+import java.io.IOException;
 public class Main {
     final static int PORT = 9999;
-
     public static void main(String[] args) {
 
         Server server = new Server();
+
+        for (String validPath : ClientHandler.validPaths){
+            server.addHandler("GET", validPath, (request, responseStream) -> {
+                try{
+                    ClientHandler.responseOK(request,responseStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        server.addHandler("POST", "/resources.html", (request, responseStream) -> {
+           try{
+               ClientHandler.responseOK(request,responseStream);
+            }catch (IOException e){
+               e.printStackTrace();
+           }
+        });
         server.startingTheServer(PORT);
     }
 }
